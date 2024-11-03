26f9e77224e7b4dce92fcad768c94a78d8587aec
sam geyer <geyerwork@gmail.com>
2024-11-03 21:41:32 +1100

    adding walls and mouse control options on click & drag

--- shapes.js
+++ shapes.js
@@ -8,7 +8,7 @@
 // const Render = Matter.Render
 
 // using one line of code instead of multi-line aliases 
-const {Engine, Render, Bodies, World} = Matter
+const {Engine, Render, Bodies, World, MouseConstraint} = Matter
 
 
 // where matter is being deployed
@@ -50,17 +50,38 @@ const bigBall = Bodies.circle(w / 2, h / 2, 250, {
     }
 })
 
-// create the ground - width, height etc is positioning to get it into the right spot
-const ground = Bodies.rectangle(w / 2, h + 50, w + 100, 100, {
+const wallOptions = {
     isStatic: true,
     render: {
         visible: false
     }
-})
+}
+
+// create the ground - width, height etc is positioning to get it into the right spot
+const ground = Bodies.rectangle(w / 2, h + 50, w + 100, 100, wallOptions)
+const ceiling = Bodies.rectangle(w / 2,  -50, w + 100, 100, wallOptions)
+const leftWall = Bodies.rectangle(-50, h / 2, 100, h + 100, wallOptions)
+const rightWall = Bodies.rectangle(w + 50, h / 2, 100, h + 100, wallOptions)
+
+
+const mouseControl = MouseConstraint.create(engine, {
+    element: sectionTag,
+    constraint: {
+        render: {
+            visible: false
+        }
+    }
+    })
+
+
 
 World.add(engine.world, [
     bigBall, 
-    ground
+    ground,
+    ceiling,
+    leftWall,
+    rightWall,
+    mouseControl
 ])
 
 
